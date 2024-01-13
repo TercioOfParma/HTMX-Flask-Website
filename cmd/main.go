1398920d337a68df0b2cff5cae9a35b3a7235b2e
Elliot Lake <gigaraptorama@gmail.com>
2024-01-13 14:57:43 +0000

    Devolve List Template and Update with Get Request

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/gorilla/mux"
 	"github.com/mousedownco/htmx-contact-app/contacts"
 	"github.com/mousedownco/htmx-contact-app/views"
-	"log"
-	"net/http"
 )
 
 var staticDir = "static"
@@ -19,9 +20,9 @@ func main() {
 		http.StripPrefix("/static/",
 			http.FileServer(http.Dir(staticDir))))
 	r.Handle("/",
-		http.RedirectHandler("/contacts", http.StatusTemporaryRedirect))
-	r.Handle("/contacts",
 		contacts.HandleIndex(cs, views.NewView("layout", "contacts/index.gohtml")))
+	r.Handle("/contacts",
+		contacts.HandleIndex(cs, views.NewView("layout", "contacts/list.gohtml")))
 	r.Handle("/contacts/new",
 		contacts.HandleNew(views.NewView("layout", "contacts/new.gohtml"))).Methods("GET")
 	r.Handle("/contacts/new",
